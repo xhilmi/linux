66cc4906d65701438cf152a0295533d1b312fa66
hilmi-afifi <38133643+hilmi-afifi@users.noreply.github.com>
2023-01-03 20:37:58 +0700

    Create install.sh

---
+++ bash/install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+clear;
+echo ""
+echo ">>>>> installing package nano <<<<<";
+sudo apt install nano -y;
+echo ""
+
+echo ">>>>> installing package wget <<<<<";
+sudo apt install wget -y;
+echo ""
+
+echo ">>>>> installing package curl <<<<<";
+sudo apt install curl -y;
+echo ""
+
+echo ">>>>> installing package tree <<<<<";
+sudo apt install tree -y;
+echo ""
+
+echo ">>>>> installing package net-tools <<<<<";
+sudo apt install net-tools -y;
+echo ""
+
+echo ">>>>> installing package fish <<<<<";
+sudo apt install fish -y;
+echo ""
