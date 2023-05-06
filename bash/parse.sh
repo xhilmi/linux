8a579d52c3ba28cbad359d3c79e58b63e5e86303
Hilmi Afifi <38133643+xhilmi@users.noreply.github.com>
2023-05-06 19:53:55 +0700

    Create parse.sh

---
+++ bash/parse.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# Define your path example log
+SYSLOG="/var/log/syslog"
+
+# Backup your example into the new one
+LOG_LOCATION="/var/log/challenge.log"
+
+# Export final result to new file
+EXPORT_LOCATION="$HOME/linux-challenge/new.log"
+
+# Specify Date
+DATE="$(date '+%b %e')"
+
+# Interactive prompt user for input
+read -p "Enter date of syslog search (leave blank for today's date): " DATE_TEXT
+
+# If there is no date text provided, use today's date
+if [[ -z "$DATE_TEXT" ]]; then
+    DATE_TEXT="$DATE"
+fi
+
+# Create challenge.log file if it doesn't exist
+if [[ ! -f "$LOG_LOCATION" ]]; then
+    touch "$LOG_LOCATION"
+fi
+
+# Append syslog into challenge.log
+cat "$SYSLOG" > "$LOG_LOCATION"
+
+# Search for date and text then replace "Error or error" with "Not Error" also export into new final result file
+grep "$DATE_TEXT" "$LOG_LOCATION" | grep "Error\|error" | sed "s/.*rror/Not Error/g" > "$EXPORT_LOCATION"
+
+# Print some text below
+echo -e "\n\e[1mSummary Report:\e[0m"
+echo -e "\e[1m----------------\e[0m"
+
+# Show latestfinal result file with color output
+while read line; do
+  # Check if the line contains "Not Error" and change color to green, otherwise color to orange
+  if [[ $line == *"Not Error"* ]]; then
+    echo -e "\e[32m$line\e[0m"
+  else
+    echo -e "\e[33m$line\e[0m"
+  fi
+done < "$EXPORT_LOCATION" | tail
